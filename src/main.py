cc46b406b9a519e3073c7051ff67efb3bf73ef96
IchWambo <fabian-oberer@web.de>
2025-09-17 02:19:15 +0200

    even more fixes

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ from generate_page import generate_page
 from generate_pages_recursive import generate_pages_recursive
 
 def main():
-    if not sys.argv[0]:
+    if not sys.argv[1]:
         basepath = "/"
-    basepath = sys.argv[0]
+    basepath = sys.argv[1]
     print("hello world")
     copy_contents("static", "docs")
     generate_pages_recursive("content", "template.html", "docs", basepath)
